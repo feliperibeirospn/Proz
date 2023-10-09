52826885ce00cfc5beafbd2acc47192ad2f83b6d
felipe <felipecleones999@gmail.com>
2023-10-09 20:15:01 -0300

    atividade de estilos

---
+++ aula-style-classList/script.js
@@ -0,0 +1,10 @@
+const login = document.querySelector('h2')
+
+login.style.color = 'blue';
+login.style.fontSize= "33px";
+
+const buttonColor = document.querySelector('button')
+
+buttonColor.style.color= 'blue';
+buttonColor.style.border= '2px solid blue';
+buttonColor.style.borderRadius = '13px';

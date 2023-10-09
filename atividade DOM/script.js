6a6e8e820860f89b886a9cf8ade87cbf86f5b2f1
felipe <felipecleones999@gmail.com>
2023-10-09 13:31:07 -0300

    add

--- atividade DOM/script.js
+++ atividade DOM/script.js
@@ -1,2 +1,24 @@
-let titulo = document.createElement(h1)
-titulo.innerText = "titulo"
+// Criando um elemento para o título
+let titulo = document.createElement("h1");
+
+titulo.id = "titulo";
+titulo.innerText = "Massa de pizza";
+
+let body = document.querySelector("body");
+
+body.appendChild(titulo);
+
+
+let produto = document.createElement("div");
+
+
+produto.innerHTML = `
+  <div>
+    <h2>Massa Grande</h2>
+    <img src="https://pin.it/14h8qZZ" alt="Pizza">
+    <p>Massa.</p>
+    <p id="preco-pizza">R$ 9.99</p>
+  </div>
+`;
+
+body.appendChild(produto)

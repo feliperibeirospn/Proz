95854a1399fc3a3aff165f4e7a877ca1e3c303ec
felipe <felipecleones999@gmail.com>
2023-10-16 21:53:00 -0300

    functions add

--- ExercicioJS-master/script.js
+++ ExercicioJS-master/script.js
@@ -1,45 +1,50 @@
 
 
-const linkPerfil = document.getElementById("link-perfil");
-const navPerfil = document.getElementById("nav-perfil");
 
 
-document.addEventListener("keyup", (e) => {
-  console.log(e.key)
-  console.log(e.code)
-
-  if(e.code == 'Digit1') {
-    console.log('Abre o menu lateral');
-    navPerfil.style.display = "block";
-  } else if(e.code == 'Escape') {
-    console.log('Fecha o menu lateral');
-    navPerfil.style.display = "none";
-  } 
-})
-
-const linkPerfilDados = document.getElementById('link-perfil-dados');
+function irpara() {
+  const linkPerfil = document.getElementById("link-perfil");
+  const navPerfil = document.getElementById("nav-perfil");
 
 
-  document.addEventListener('keyup', (e) =>{
-    if(navPerfil.style.display== 'block'){
-      if(e.code = 'Digit1'){
+  document.addEventListener("keyup", (e) => {
+    console.log(e.key)
+    console.log(e.code)
+
+    if (e.code == 'Digit1') {
+      console.log('Abre o menu lateral');
+      navPerfil.style.display = "block";
+    } else if (e.code == 'Escape') {
+      console.log('Fecha o menu lateral');
+      navPerfil.style.display = "none";
+    }
+  })
+  const linkPerfilDados = document.getElementById('link-perfil-dados');
+
+
+  document.addEventListener('keyup', (e) => {
+    if (navPerfil.style.display == 'block') {
+      if (e.code = 'Digit1') {
         linkPerfilDados.click()
       }
-    }else if(e.code =='Digit1'){
+    } else if (e.code == 'Digit1') {
       navPerfil.style.display = 'block'
     }
   })
+}
 
- 
-    const voltar = document.querySelector('p a .home-casa');
 
-    document.addEventListener('keyup', (e)=>{
-    if(e.code == 'Digit3'){
+
+function fechar() {
+  document.addEventListener('keyup', (e) => {
+    if (e.code == 'Digit3') {
       window.location.href = 'index.html';
     }
-   })
-  
-  
- 
+  })
+}
+fechar();
+irpara();
 
-  
+
+
+
